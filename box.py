ba3cafd2ba9bdbd95116acf99e714206485bf5b1
radioGiorgio <florent.delgrange@gmail.com>
2016-08-01 13:54:43 +0200

    Text is now centered

--- box.py
+++ box.py
@@ -15,7 +15,7 @@ class Box:
     """
 
 
-    def __init__(self, yaml_dict, horizontal_axis=False, parallel_state=False):
+    def __init__(self, yaml_dict, horizontal_axis=True, parallel_state=False):
         self.name = yaml_dict['name']
         self.children = []
         self.horizontal_axis = horizontal_axis
@@ -29,12 +29,12 @@ class Box:
         if 'states' in yaml_dict:
             self.children += [Box(x, not(horizontal_axis)) for x in yaml_dict['states']]
         if parallel_state:
-            p_len = 13 * char_width
+            p_len = 14 * char_width
         else:
             p_len = 0
         if horizontal_axis:
             self.width = max(sum(map(lambda x: x.width + space, self.children)) + space, space + p_len + char_width * len(self.name) + space)
-            self.height = max(list(map(lambda x: x.height, self.children)) or [0]) + 4 * space + char_height
+            self.height = max(list(map(lambda x: x.height, self.children)) or [0]) + 3 * space + char_height
         else:
             self.width = max(max(list(map(lambda x: x.width, self.children)) or [0]) + 2 * space, space + p_len + char_width * len(self.name) + space)
             self.height = sum(map(lambda x: x.height + space, self.children)) + space + 2 * space + char_height
@@ -47,8 +47,8 @@ class Box:
         :param dwg: Drawing object obtained from svgwrite
         :param insert: upper left corner position of the box
         """
-        normal_style = "font-size:20;font-family:Arial"
-        italic_style = "font-size:20;font-family:Arial;font-style:oblique"
+        normal_style = "font-size:25;font-family:Arial"
+        italic_style = "font-size:25;font-family:Arial;font-style:oblique"
         x, y = insert
         # First draw the main box
         rect = dwg.rect(insert=(x, y), size=(self.width, self.height), fill=svgwrite.rgb(135,206,235), stroke='black', stroke_width=1)
@@ -58,20 +58,20 @@ class Box:
         h = y + space + char_height
         if self.parallel_state:
             w = w - (len(self.name) * char_width + 13 * char_width) / 2
-            t1 = dwg.text("<<parallel state>>", insert=(w, h), style=italic_style)
-            t2 = dwg.text(self.name, insert=(w + 13 * char_width, h), style=normal_style)
+            t1 = dwg.text("<<parallel>>", insert=(w, h), style=italic_style, textLength=13*char_width)
+            t2 = dwg.text(self.name, insert=(w + 14 * char_width, h), style=normal_style, textLength=len(self.name)*char_width)
             dwg.add(t1)
             dwg.add(t2)
         else:
             w = w - (len(self.name) * char_width) / 2
-            dwg.add(dwg.text(self.name, insert=(w, h), style=normal_style))
+            dwg.add(dwg.text(self.name, insert=(w, h), style=normal_style, textLength=len(self.name) * char_width))
         # Finnaly draw the children following the axis (horizontal or vertical)
         if self.horizontal_axis:
             w = x
-            h = y + self.height/2
+            h = y + 2 * space + char_height + (self.height - 2 * space - char_height) / 2
             for child in self.children:
                 w += space
-                child.render(dwg, insert=(w, h - child.height / 2))
+                child.render(dwg, insert=(w, h - child.height/2))
                 w += child.width
         else:
             w = x + self.width/2
@@ -79,7 +79,7 @@ class Box:
             for child in self.children:
                 h += space
                 child.render(dwg, insert=(w - child.width/2, h))
-                h += child.height
+                h += child.height 
 
 
     def export(self):
