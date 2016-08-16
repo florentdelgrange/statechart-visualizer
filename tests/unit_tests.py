c8d23aab3d98621f8371101fa46d39e8a5c8796c
radioGiorgio <florent.delgrange@gmail.com>
2016-08-16 17:11:08 +0200

    segment computing for intersection between transitions

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,9 +1,32 @@
 from sismic import io
 import sismic
 import unittest
-
+from structures.segment import Segment, intersect, combined_segments
 from constraint_solver import Constraint
 from structures.box_elements import RootBox
+
+
+class TestSegment(unittest.TestCase):
+    def test_simple_intersection(self):
+        s1 = Segment((0, 2), (4, 2))
+        s2 = Segment((0, 0), (4, 4))
+        self.assertEqual(intersect(s1, s2), (2, 2))
+
+    def test_orthogonal_intersection(self):
+        s1 = Segment((0, 2), (4, 2))
+        s2 = Segment((2, 4), (2, 0))
+        self.assertEqual(intersect(s1, s2), (2, 2))
+
+    def test_combined_segments(self):
+        combined = combined_segments(Segment((1, 3), (1, 6)), Segment((1, 4), (1, 7)))
+        p1, p2 = combined.p1, combined.p2
+        self.assertEqual(p1, (1, 4))
+        self.assertEqual(p2, (1, 6))
+        p = intersect(Segment((1, 3), (1, 6)), Segment((1, 4), (1, 7)))
+        self.assertEqual(p, (1, (4, 6)))
+        combined = combined_segments(Segment((3, 1), (6, 1)), Segment((4, 1), (7, 1)))
+        self.assertEqual(combined.p1, (4, 1))
+        self.assertEqual(combined.p2, (6, 1))
 
 
 class TestBoxElements(unittest.TestCase):
